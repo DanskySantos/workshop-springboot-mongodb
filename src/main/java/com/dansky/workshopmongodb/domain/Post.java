6ad480dca6f7ce60c3673d576bfcf8a8bd005998
Dansky Santos <dansky123@hotmail.com>
2022-03-18 22:15:27 -0300

    acrescentando comentarios aos posts

--- src/main/java/com/dansky/workshopmongodb/domain/Post.java
+++ src/main/java/com/dansky/workshopmongodb/domain/Post.java
@@ -1,15 +1,17 @@
 package com.dansky.workshopmongodb.domain;
 
 import com.dansky.workshopmongodb.dto.AuthorDTO;
+import com.dansky.workshopmongodb.dto.CommentDTO;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode
@@ -22,4 +24,14 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
+
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
+        this.id = id;
+        this.date = date;
+        this.title = title;
+        this.body = body;
+        this.author = author;
+    }
 }
