882a8bf263737c6fce544f7e955c3260c4a884e6
Dansky Santos <dansky123@hotmail.com>
2022-03-18 22:44:30 -0300

    consulta simples com query methods

--- src/main/java/com/dansky/workshopmongodb/resources/PostResource.java
+++ src/main/java/com/dansky/workshopmongodb/resources/PostResource.java
@@ -4,13 +4,13 @@ import com.dansky.workshopmongodb.domain.Post;
 import com.dansky.workshopmongodb.domain.User;
 import com.dansky.workshopmongodb.dto.AuthorDTO;
 import com.dansky.workshopmongodb.dto.UserDTO;
+import com.dansky.workshopmongodb.resources.util.URL;
 import com.dansky.workshopmongodb.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -23,5 +23,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post post = postService.findById(id);
         return ResponseEntity.ok().body(post);
+    }
+
+    @GetMapping(value = "titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/dansky/workshopmongodb/services/PostService.java
+++ src/main/java/com/dansky/workshopmongodb/services/PostService.java
@@ -1,12 +1,12 @@
 package com.dansky.workshopmongodb.services;
 
 import com.dansky.workshopmongodb.domain.Post;
-import com.dansky.workshopmongodb.domain.User;
 import com.dansky.workshopmongodb.repository.PostRepository;
 import com.dansky.workshopmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,5 +18,9 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> post = postRepository.findById(id);
         return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return postRepository.findByTitleContainingIgnoreCase(text);
     }
 }
